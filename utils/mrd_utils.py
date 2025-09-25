4277687774427f42e543ff7e45d642b25f7b35b6
sl609@duke.edu <sl609@duke.edu>
2025-09-24 21:50:18 -0400

    Fix trajectory writer

--- utils/mrd_utils.py
+++ utils/mrd_utils.py
@@ -111,6 +111,9 @@ def write_acquisition_data(path: str, data_dict: Dict[str, Any]):
 
     # initialize ismrmrd data set object
     ismrmrd_data_set = ismrmrd.Dataset(path, "dataset", create_if_needed=True)
+    # initialize counter for imaging trajectory
+    acquisition_num_imaging = 0;
+
     # write to acquisition and acquisition header for each FID acquisition
     for acquisition_num in range(data_dict[constants.IOFields.FIDS].shape[0]):
         # initialize acquisition object and acquisition header
@@ -118,7 +121,6 @@ def write_acquisition_data(path: str, data_dict: Dict[str, Any]):
         acquisition_header = ismrmrd.AcquisitionHeader()
 
         # write acquisition header
-        acquisition_header.number_of_samples = data_dict[constants.IOFields.N_POINTS]
         acquisition_header.active_channels = 1
         acquisition_header.trajectory_dimensions = 3
         acquisition_header.idx.contrast = int(
@@ -149,6 +151,7 @@ def write_acquisition_data(path: str, data_dict: Dict[str, Any]):
         if (int(data_dict[constants.IOFields.BONUS_SPECTRA_LABELS][acquisition_num])==0):
             acquisition_header.sample_time_us = data_dict[constants.IOFields.SAMPLE_TIME]
             n_points_writing = data_dict[constants.IOFields.N_POINTS]
+            acquisition_header.number_of_samples = n_points_writing 
             acquisition.resize(n_points_writing, 1)
             acquisition.data[:] = data_dict[constants.IOFields.FIDS][acquisition_num, :n_points_writing]
 
@@ -162,11 +165,10 @@ def write_acquisition_data(path: str, data_dict: Dict[str, Any]):
         # set acquisition header
         acquisition.setHead(acquisition_header)
         # write acquisition trajectory data
-        if constants.IOFields.TRAJ in data_dict:
-            if acquisition_num < data_dict[constants.IOFields.TRAJ].shape[0]:
-                acquisition.traj[:] = data_dict[constants.IOFields.TRAJ][
-                    acquisition_num, :, :
-                ]
+        if (int(data_dict[constants.IOFields.BONUS_SPECTRA_LABELS][acquisition_num])==0):
+            acquisition.traj[:] = data_dict[constants.IOFields.TRAJ][acquisition_num_imaging, :, :]
+            acquisition_num_imaging= acquisition_num_imaging+1;
+
 
         # append aquisition to ismrmrd data object
         ismrmrd_data_set.append_acquisition(acquisition)
